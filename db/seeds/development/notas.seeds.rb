a21a28ff786654c152073f8a74488186a1c93c37
Marcos Neves <marcos.neves@gmail.com>
2015-06-20 01:35:55 -0300

    seed notas

---
+++ db/seeds/development/notas.seeds.rb
@@ -0,0 +1,10 @@
+user = User.first
+
+25.times do
+  Nota.create! do |n|
+    n.user = user
+    n.titulo = Faker::Lorem.sentence
+    n.texto = Faker::Lorem.paragraphs.join "\n"
+    n.created_at = n.updated_at = Faker::Date.backward
+  end
+end
