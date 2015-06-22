a2a58de9f1ce8c1d8de5edf696b7db445b927def
Marcos Neves <marcos.neves@gmail.com>
2015-06-22 18:42:04 -0300

    titulo deve ser unico

--- app/models/nota.rb
+++ app/models/nota.rb
@@ -2,4 +2,5 @@ class Nota < ActiveRecord::Base
   belongs_to :user
   default_scope { order 'updated_at DESC' }
   validates :titulo, :texto, presence: true
+  validates :titulo, uniqueness: {case_sensitive: false, scope: :user}
 end
