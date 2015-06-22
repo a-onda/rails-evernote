0501cad3fa5839f9c597e9ec4f932d8ac023b30a
Marcos Neves <marcos.neves@gmail.com>
2015-06-22 00:06:06 -0300

    add host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'evernote.herokuapp.com' }
   config.action_mailer.delivery_method   = :postmark
   config.action_mailer.postmark_settings = { :api_token => ENV['POSTMARK_API_TOKEN'] }
   # Settings specified here will take precedence over those in config/application.rb.
