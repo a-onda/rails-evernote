7aa13d2a25d03e3e45a51a7f6c661a595a9b97d8
Marcos Neves <marcos.neves@gmail.com>
2015-06-22 18:43:12 -0300

    paginar lista de notas com apenas 5 por página

--- app/models/nota.rb
+++ app/models/nota.rb
@@ -3,4 +3,5 @@ class Nota < ActiveRecord::Base
   default_scope { order 'updated_at DESC' }
   validates :titulo, :texto, presence: true
   validates :titulo, uniqueness: {case_sensitive: false, scope: :user}
+  paginates_per 5
 end
