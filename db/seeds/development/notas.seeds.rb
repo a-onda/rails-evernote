aff99d52195ce59f1a04aeb36a4182ea26221c4e
Marcos Neves <marcos.neves@gmail.com>
2015-07-25 11:35:10 -0300

    tornar seed de notas dependente de users

--- db/seeds/development/notas.seeds.rb
+++ db/seeds/development/notas.seeds.rb
@@ -1,10 +1,12 @@
-user = User.first
+after 'development:users' do
+  user = User.first
 
-25.times do
-  Nota.create! do |n|
-    n.user = user
-    n.titulo = Faker::Lorem.sentence
-    n.texto = Faker::Lorem.paragraphs.join "\n"
-    n.created_at = n.updated_at = Faker::Date.backward
+  25.times do
+    Nota.create! do |n|
+      n.user = user
+      n.titulo = Faker::Lorem.sentence
+      n.texto = Faker::Lorem.paragraphs.join "\n"
+      n.created_at = n.updated_at = Faker::Date.backward
+    end
   end
 end
